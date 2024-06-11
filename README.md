133810e58d4b48d4a00c835a376e7cccfab4e8c7
Jeongwon, Bae <bjw020615@gmail.com>
2024-06-12 00:29:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## GPTranslate Backend
+
+그냥 매우 심플하게 OpenAI API Fetch하는 코드를 작성하였음.
+현재는 Python으로 함수 하나만 작성하여 AWS Lambda를 통해 서버리스로 API 호스팅 중.
+
+추후 문서 번역, 영상 번역, 사진 번역(OCR) 등도 확장해볼 생각이라,
+Flask 혹은 FastAPI로 확장 가능하다는 생각으로 짰습니다.
---
+++ gpt_answer_lambda.py
@@ -0,0 +1,31 @@
+from http import HTTPStatus
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+
+SYSTEM_PROMPT = """You are a helpful translator. You should translate the following text to the requested language.
+And you shouldn't provide any other descriptions or information about the text. You MUST give ONLY translated text."""
+PROMPT = "Translate the following text to "
+
+def fetchAPI(api_key, lang, source_text):
+    client = OpenAI()
+    prompt = PROMPT + lang + ": " + source_text
+
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    return {"status": HTTPStatus.OK, "body": completion.choices[0].message}
+
+if __name__ == "__main__":
+    # Load environment variables
+    load_dotenv()
+
+    api_key = os.getenv("OPENAI_API_KEY")
+    val = fetchAPI(api_key, "Spanish", "Hello, how are you?")
+
+    print(val)

499a0fa3e3a3c93d3a7248a097fc36c79f0498f0
Jeongwon, Bae <bjw020615@gmail.com>
2024-06-12 02:20:57 +0900

    Lambda fit

--- gpt_answer_lambda.py
+++ gpt_answer_lambda.py
@@ -1,17 +1,25 @@
-from http import HTTPStatus
-from dotenv import load_dotenv
-import os
 from openai import OpenAI
+from http import HTTPStatus
+import json
 
-SYSTEM_PROMPT = """You are a helpful translator. You should translate the following text to the requested language.
-And you shouldn't provide any other descriptions or information about the text. You MUST give ONLY translated text."""
-PROMPT = "Translate the following text to "
+import os
+from dotenv import load_dotenv
 
-def fetchAPI(api_key, lang, source_text):
+def lambda_handler(event, context):
+    SYSTEM_PROMPT = """You are a helpful translator. You should translate the following text to the requested language.
+    And you shouldn't provide any other descriptions or information about the text. You MUST give ONLY translated text."""
+    PROMPT = "Translate the following text to "
+    
+    eventbody = event["body"]
+    
+    api_key = eventbody["api_key"]
+    lang = eventbody["lang"]
+    source_text = eventbody["source_text"]
+
     try:
-        client = OpenAI()
+        client = OpenAI(api_key = api_key)
         prompt = PROMPT + lang + ": " + source_text
-
+    
         completion = client.chat.completions.create(
             model="gpt-4o",
             messages=[
@@ -20,16 +28,24 @@ def fetchAPI(api_key, lang, source_text):
             ]
         )
         
-        return {"status": HTTPStatus.OK, "data": completion.choices[0].message.content}
+        return {"statusCode": HTTPStatus.OK, "body": completion.choices[0].message.content}
     
     except Exception as e:
-        return {"status": HTTPStatus.INTERNAL_SERVER_ERROR, "message": str(e)}
+        return {"statusCode": HTTPStatus.INTERNAL_SERVER_ERROR, "message": str(e)}
 
 if __name__ == "__main__":
     # Load environment variables
     load_dotenv()
 
     api_key = os.getenv("OPENAI_API_KEY")
-    val = fetchAPI(api_key, "Spanish", "Hello, how are you?")
+    event = {
+        "body": {
+            "api_key": api_key,
+            "lang": "Spanish",
+            "source_text": "Hello, how are you?"
+        }
+    }
+
+    val = lambda_handler(event, None)
 
     print(val)

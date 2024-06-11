fac9f05b3368cf1b97a7707b9f85f28e9339f507
Jeongwon, Bae <bjw020615@gmail.com>
2024-06-12 00:32:23 +0900

    2nd commit

--- gpt_answer_lambda.py
+++ gpt_answer_lambda.py
@@ -8,18 +8,22 @@ And you shouldn't provide any other descriptions or information about the text. 
 PROMPT = "Translate the following text to "
 
 def fetchAPI(api_key, lang, source_text):
-    client = OpenAI()
-    prompt = PROMPT + lang + ": " + source_text
+    try:
+        client = OpenAI()
+        prompt = PROMPT + lang + ": " + source_text
 
-    completion = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "system", "content": SYSTEM_PROMPT},
-            {"role": "user", "content": prompt}
-        ]
-    )
-
-    return {"status": HTTPStatus.OK, "body": completion.choices[0].message}
+        completion = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        
+        return {"status": HTTPStatus.OK, "data": completion.choices[0].message.content}
+    
+    except Exception as e:
+        return {"status": HTTPStatus.INTERNAL_SERVER_ERROR, "message": str(e)}
 
 if __name__ == "__main__":
     # Load environment variables
